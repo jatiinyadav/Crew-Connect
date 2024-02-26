b4ba1c7bd03128380a5ada56cd06d517b2f40aa9
Jatin Yadav <jatin27yadav@gmail.com>
2024-02-26 18:13:22 +0530

    Added Change Chat Theme Feature

--- src/app/components/_baseComponents/header/header.component.ts
+++ src/app/components/_baseComponents/header/header.component.ts
@@ -2,6 +2,13 @@ import { Component, Input, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChatService } from 'src/app/services/chat.service';
 
+const images = [
+  'https://images.unsplash.com/photo-1535332371349-a5d229f49cb5?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1517160990988-82edf8b37188?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1507502707541-f369a3b18502?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1529061333663-08226b29f6ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+];
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -12,6 +19,31 @@ export class HeaderComponent {
   @Input() show_button = false;
   router = inject(Router)
   chatService = inject(ChatService)
+  imageUrls = images;
+  showDropdown = false;
+  hoveredImageUrl = '';
+  previousImage = ""
+  selectedImageIndex = -1; // Index of the selected image, initially set to -1 (no image selected)
+
+  selectImage(index: number): void {
+    this.selectedImageIndex = index; // Store the index of the clicked image
+  }
+
+  toggleDropdown(): void {
+    this.previousImage = this.chatService.hoveredImageUrl
+    this.showDropdown = !this.showDropdown;
+  }
+
+  discardImage(): void {
+    this.chatService.hoveredImageUrl = this.previousImage
+    this.showDropdown = !this.showDropdown;
+  }
+
+  setHoveredImageUrl(url: string): void {
+    if (this.selectedImageIndex === -1) {
+      this.chatService.hoveredImageUrl = url;
+    }
+  }
 
   leaveChat(back_to_home: boolean) {
     const result = window.confirm('Are you sure you want leave the chat?');
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -13,6 +13,7 @@ export class ChatService {
   public groupName = "";
   public imageURL = "";
   public isGroupJoined = false;
+  public hoveredImageUrl = "https://images.unsplash.com/photo-1535332371349-a5d229f49cb5?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   public all_messages: Group[] = []
   public all_online_users: string[] = []
