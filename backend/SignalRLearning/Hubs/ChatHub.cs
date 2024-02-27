81b0ae5bf8d521099c010b1265f469ef545b9bd2
Jatin Yadav <jatin27yadav@gmail.com>
2024-02-28 02:44:01 +0530

    Code comments Added

--- backend/SignalRLearning/Hubs/ChatHub.cs
+++ backend/SignalRLearning/Hubs/ChatHub.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.SignalR;
 using SignalRLearning.ChatInterfaces;
 using MongoDLL;
-using System;
 
 namespace SignalRLearning.Hubs
 {
   public class ChatHub : Hub
   {
+    /// <summary>
+    /// Class to execute all the methods
+    /// </summary>
     private readonly ChatClass _chatClass;
 
     public ChatHub(ChatClass chatClass)
@@ -14,37 +16,65 @@ namespace SignalRLearning.Hubs
       _chatClass = chatClass;
     }
 
+     /// <summary>
+     /// Will be executed when user will Join the Group.
+     /// </summary>
+     /// <param name="userDetails"></param>
+     /// <param name="userMessage"></param>
+     /// <returns></returns>
     public async Task JoinGroup(UserDetails userDetails, UserMessage userMessage)
     {
       await _chatClass.UserJoinedGroup(Groups, Clients, Context.ConnectionId, userDetails, userMessage);
     }
 
-    // Method Invoked when user sends a message
+    /// <summary>
+    /// Will be excuted when user will send a message.
+    /// </summary>
+    /// <param name="userMessage"></param>
+    /// <returns></returns>
     public async Task SendMessage(UserMessage userMessage)
     {
       await _chatClass.AddMessageToDB(Clients, Context.ConnectionId, userMessage);
     }
 
-    // Method Invoked when user got Disconnected
+    /// <summary>
+    /// Will be executed when user will leave the chat.
+    /// </summary>
+    /// <param name="exception"></param>
+    /// <returns></returns>
     public override Task OnDisconnectedAsync(Exception? exception)
     {
       _chatClass.UserExitFromChat(Clients, Context.ConnectionId, exception);
       return base.OnDisconnectedAsync(exception);
     }
 
-    // Send All the Messages
+    /// <summary>
+    /// Get all the message of that particular group.
+    /// </summary>
+    /// <param name="userMessage"></param>
+    /// <returns></returns>
     public async Task<List<Group>> SendAllMessages(UserMessage userMessage)
     {
       return await _chatClass.GetAllGroupMessages(userMessage);
     }
 
-    // Get All Collection Name from Database
+    /// <summary>
+    /// Will be executed at the time of login.
+    /// To check whether the group exits or not.
+    /// </summary>
+    /// <param name="adminName"></param>
+    /// <param name="userMessage"></param>
+    /// <param name="createGroup"></param>
+    /// <returns></returns>
     public async Task<Boolean> GetCollectionsNames(string adminName, UserMessage userMessage, bool createGroup)
     {
       return await _chatClass.CollectionsFromDB(Clients, Context.ConnectionId, adminName, userMessage, createGroup);
     }
 
-    // When Message is sent update logged in user
+    /// <summary>
+    /// To get the logged in user username.
+    /// </summary>
+    /// <param name="username"></param>
     public void UpdateUser(string username)
     {
       _chatClass.GetLoggedInUser(Clients, Context.ConnectionId, username);
